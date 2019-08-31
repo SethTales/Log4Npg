58409f6eeff36aa0f57a14b5b0b52560453a65f7
Seth <seth.tales@gmail.com>
2019-08-31 13:57:30 -0700

    Added a new constructor param to the NpgLogger class to set a default log level. Also added a private method that checks agains this default log level before logging, to make sure it is logging at the correct verbosity. Alos added unit tests to verify this new functionality

--- Log4Npg.Logging/NpgLogger.cs
+++ Log4Npg.Logging/NpgLogger.cs
@@ -8,35 +8,56 @@ namespace Log4Npg.Logging
     public class NpgLogger : INpgLogger
     {
         private readonly ILoggingRepository _loggingRepository;
-        public NpgLogger(ILoggingRepository loggingRepository)
+        private readonly LogLevel _logLevel;
+        public NpgLogger(ILoggingRepository loggingRepository, LogLevel logLevel)
         {
             _loggingRepository = loggingRepository;
+            _logLevel = logLevel;
         }
 
         public void LogDebug(object message)
         {
-            var logEntry = BuildLogEntry(message, LogLevel.Debug);
-            _loggingRepository.AddLogEntry(logEntry);
+            if (CanLogAtLevel(LogLevel.Debug))
+            {
+                var logEntry = BuildLogEntry(message, LogLevel.Debug);
+                _loggingRepository.AddLogEntry(logEntry);
+            }
         }
+
         public void LogInfo(object message)
         {
-            var logEntry = BuildLogEntry(message, LogLevel.Info);
-            _loggingRepository.AddLogEntry(logEntry);
+            if (CanLogAtLevel(LogLevel.Info))
+            {
+                var logEntry = BuildLogEntry(message, LogLevel.Info);
+                _loggingRepository.AddLogEntry(logEntry);
+            }
         }
+
         public void LogWarning(object message)
         {
-            var logEntry = BuildLogEntry(message, LogLevel.Warn);
-            _loggingRepository.AddLogEntry(logEntry);
+            if (CanLogAtLevel(LogLevel.Warn))
+            {
+                var logEntry = BuildLogEntry(message, LogLevel.Warn);
+                _loggingRepository.AddLogEntry(logEntry);
+            }
         }
+
         public void LogError(object message)
         {
-            var logEntry = BuildLogEntry(message, LogLevel.Error);
-            _loggingRepository.AddLogEntry(logEntry);
+            if (CanLogAtLevel(LogLevel.Error))
+            {
+                var logEntry = BuildLogEntry(message, LogLevel.Error);
+                _loggingRepository.AddLogEntry(logEntry);
+            }
         }
+
         public void LogFatal(object message)
         {
-            var logEntry = BuildLogEntry(message, LogLevel.Fatal);
-            _loggingRepository.AddLogEntry(logEntry);
+            if (CanLogAtLevel(LogLevel.Fatal))
+            {
+                var logEntry = BuildLogEntry(message, LogLevel.Fatal);
+                _loggingRepository.AddLogEntry(logEntry);
+            }
         }
 
         private LogEntry BuildLogEntry(object message, LogLevel level)
@@ -47,6 +68,11 @@ namespace Log4Npg.Logging
                 EventTime = DateTime.UtcNow,
                 Message = JsonConvert.SerializeObject(message)
             };
+        }
+
+        private bool CanLogAtLevel(LogLevel level)
+        {
+            return level >= _logLevel;
         }
     }
 }
--- Log4Npg.Tests/UnitTests/LoggerTests.cs
+++ Log4Npg.Tests/UnitTests/LoggerTests.cs
@@ -3,6 +3,8 @@ using NSubstitute;
 using Log4Npg.Logging.Data;
 using Log4Npg.Logging;
 using Log4Npg.Models;
+using System.Reflection;
+using System.Collections.Generic;
 
 namespace Log4Npg.Tests.UnitTests
 {
@@ -10,13 +12,14 @@ namespace Log4Npg.Tests.UnitTests
     public class LoggerTests
     {
         private ILoggingRepository _loggingResitory;
-        private INpgLogger _logger;
+        private INpgLogger _defaultLogger;
+        private string[] _logMethodNames = new[] {"LogDebug", "LogInfo", "LogWarning", "LogError", "LogFatal"};
 
         [SetUp]
         public void Setup()
         {
             _loggingResitory = Substitute.For<ILoggingRepository>();
-            _logger = new NpgLogger(_loggingResitory);
+            _defaultLogger = new NpgLogger(_loggingResitory, LogLevel.All);
         }
 
         [TestCase("LogDebug", LogLevel.Debug)]
@@ -26,12 +29,39 @@ namespace Log4Npg.Tests.UnitTests
         [TestCase("LogFatal", LogLevel.Fatal)]
         public void EachLoggingMethod_CallsLoggingRepository_WithCorrectLogLevelEnum(string methodName, LogLevel level)
         {
-            var type = _logger.GetType();
+            var type = _defaultLogger.GetType();
             var logMethod = type.GetMethod(methodName);
             var logMessage = "this is a test message";
-            logMethod.Invoke(_logger, new object[] {logMessage});
+
+            logMethod.Invoke(_defaultLogger, new object[] {logMessage});
+
             _loggingResitory.Received(1).AddLogEntry(Arg.Is<LogEntry>(x =>
                 x.Level == level));
+        }
+
+        [TestCase(LogLevel.All, 5)]
+        [TestCase(LogLevel.Debug, 5)]
+        [TestCase(LogLevel.Info, 4)]
+        [TestCase(LogLevel.Warn, 3)]
+        [TestCase(LogLevel.Error, 2)]
+        [TestCase(LogLevel.Fatal, 1)]
+        public void NpgLogger_LogsAtCorrectLevel_BasedOnLogLevel_SetInConstructor(LogLevel baseLogLevel, int expectedAddLogEntryInvocations)
+        {
+            var restrictedLogger = new NpgLogger(_loggingResitory, baseLogLevel);
+            var type = _defaultLogger.GetType();
+            var logMethods = new List<MethodInfo>();
+            foreach (var methodName in _logMethodNames)
+            {
+                logMethods.Add(type.GetMethod(methodName));
+            }
+            var logMessage = "this is a test message";
+
+            foreach (var method in logMethods)
+            {
+                method.Invoke(restrictedLogger, new object[] {logMessage});
+            }
+
+            _loggingResitory.Received(expectedAddLogEntryInvocations).AddLogEntry(Arg.Any<LogEntry>());
         }
 
     }
